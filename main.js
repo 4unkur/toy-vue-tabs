72db873323e32755599715a435ea6d065fccccb0
Daiyrbek Artelov <4unkur@mail.ru>
2018-09-13 08:50:12 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+Vue.component('tabs', {
+	template: `
+<div>
+	<div class="tabs">
+	  <ul>
+	  	<li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+	  		<a href="#" @click="selectTab(tab)">{{ tab.title }}</a>
+		</li>
+	  </ul>
+	</div>
+	<div class="tabs-content">
+		<slot></slot>
+	</div>
+</div>`,
+	data() {
+		return {
+			tabs: []
+		}
+	},
+	created() {
+		this.tabs = this.$children
+	},
+	methods: {
+		selectTab(selectedTab) {
+			this.tabs.forEach(tab => {
+				tab.isActive = (tab.title == selectedTab.title)
+			})
+		}
+	}
+})
+
+Vue.component('tab', {
+	template: `
+<div v-show="isActive">
+	<slot></slot>
+</div>`,
+	props: {
+		title: {
+			required: true
+		},
+		selected: {
+			default: false
+		}
+	},
+	data() {
+		return {
+			isActive: false
+		}
+	},
+	mounted() {
+		this.isActive = this.selected
+	}
+})
+
+const app = new Vue({
+	el: '#app'
+})
